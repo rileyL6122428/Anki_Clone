4dcc41ff770307beddb199ed97ec24f962ad427f
Riley Littlefield <rileylittlefield@ymail.com>
2016-06-10 11:36:01 -0400

    .

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module SampleProjectProposalMaster
   class Application < Rails::Application
+
+    config.assets.paths << Rails.route.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
