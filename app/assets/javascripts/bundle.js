6a75703a8a36d219b0e9ae0be7b72e04fb944d41
Riley Littlefield <rileylittlefield@ymail.com>
2016-06-14 20:30:37 -0400

    improves search engine for cards and decks

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -34137,16 +34137,32 @@
 
 	module.exports = {
 	  search: function (collection, attribute, searchInput) {
+	    if (searchInput === "") {
+	      return this.arrayCollection(collection);
+	    }
 	    var matches = [];
+	
 	    for (var key in collection) {
 	      var element = collection[key];
+	      var elementAttribute = element[attribute].toLowerCase();
+	      var lcSearchInput = searchInput.toLowerCase();
 	
-	      if (element[attribute].startsWith(searchInput)) {
+	      if (elementAttribute.match(lcSearchInput)) {
 	        matches.push(element);
 	      }
 	    }
 	
 	    return matches;
+	  },
+	
+	  arrayCollection: function (collection) {
+	    var arrCollection = [];
+	
+	    for (var key in collection) {
+	      arrCollection.push(collection[key]);
+	    }
+	
+	    return arrCollection;
 	  }
 	};
 
--- frontend/stores/index_search_util.js
+++ frontend/stores/index_search_util.js
@@ -1,12 +1,26 @@
 module.exports =  {
   search: function(collection, attribute, searchInput ) {
+    if(searchInput === "") { return this.arrayCollection(collection) }
     var matches = [];
-    for(var key in collection) {
-      var element =　collection[key];
 
-      if(element[attribute].startsWith(searchInput)) { matches.push(element); }
+    for(var key in collection) {
+      var element = collection[key]
+      var elementAttribute =　element[attribute].toLowerCase();
+      var lcSearchInput = searchInput.toLowerCase()
+
+      if(elementAttribute.match(lcSearchInput)) { matches.push(element); }
     }
 
     return matches;
+  },
+
+  arrayCollection: function (collection) {
+    var arrCollection = []
+
+    for(var key in collection) {
+      arrCollection.push(collection[key])
+    }
+
+    return arrCollection
   }
 };
